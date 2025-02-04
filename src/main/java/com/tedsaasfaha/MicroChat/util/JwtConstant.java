49eb0f0e9dec65a818df820d2ebb528cb4bbaae6
Tedros Alem <tedsaasfaha@gmail.com>
2025-02-04 17:44:10 +0300

    implement and add JwtConstant class for defining Jwt Secret_key & expiration validation

--- src/main/java/com/tedsaasfaha/MicroChat/util/JwtConstant.java
+++ src/main/java/com/tedsaasfaha/MicroChat/util/JwtConstant.java
@@ -1,4 +1,12 @@
 package com.tedsaasfaha.MicroChat.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class JwtConstant {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    public static final String SECRET_KEY = dotenv.get("JWT+_SECRET_KEY");
+    public static final long ACCESS_TOKEN_VALIDITY = 2 * 60 * 60 * 1000; // 2 hrs
+    public static final long REFRESH_TOKEN_VALIDITY = 12 * 60 * 60 * 1000; // 12 hrs
 }
+
