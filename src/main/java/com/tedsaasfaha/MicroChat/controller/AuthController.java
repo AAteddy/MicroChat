10e970089943dae097261f66455b15f226ed1219
Tedros Alem <tedsaasfaha@gmail.com>
2025-02-06 22:20:23 +0300

    implement refreshToken method endpoint for handling user refreshing an access token.

--- src/main/java/com/tedsaasfaha/MicroChat/controller/AuthController.java
+++ src/main/java/com/tedsaasfaha/MicroChat/controller/AuthController.java
@@ -6,16 +6,10 @@ import com.tedsaasfaha.MicroChat.dto.AuthResponseDTO;
 import com.tedsaasfaha.MicroChat.dto.UserRegistrationDTO;
 import com.tedsaasfaha.MicroChat.model.User;
 import com.tedsaasfaha.MicroChat.service.UserService;
-import com.tedsaasfaha.MicroChat.util.JwtUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,6 +45,13 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
 
-    // todo - refresh-token method endpoint
+    @PostMapping("/refresh-token")
+    public ResponseEntity<AuthResponseDTO> refreshToken(
+            @RequestBody AuthResponseDTO responseDTO
+    ) throws Exception {
+
+        AuthResponseDTO response = userService.createAuthRefreshToken(responseDTO);
+        return ResponseEntity.ok(response);
+    }
 
 }
