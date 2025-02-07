6695f90028868099e88daa1cf196b6ae83db6164
Tedros Alem <tedsaasfaha@gmail.com>
2025-02-07 15:06:29 +0300

    implement & update getAllUsers method to take Pageable (page number) as parameter & returns Page<UserResponseDTO>.

--- src/main/java/com/tedsaasfaha/MicroChat/service/UserService.java
+++ src/main/java/com/tedsaasfaha/MicroChat/service/UserService.java
@@ -1,4 +1,26 @@
+
 package com.tedsaasfaha.MicroChat.service;
 
+import com.tedsaasfaha.MicroChat.dto.AuthRequestDTO;
+import com.tedsaasfaha.MicroChat.dto.AuthResponseDTO;
+import com.tedsaasfaha.MicroChat.dto.UserResponseDTO;
+import com.tedsaasfaha.MicroChat.dto.UserUpdateDTO;
+import com.tedsaasfaha.MicroChat.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface UserService {
+
+    boolean isExist(String email);
+
+    User registerUser(User user);
+
+    UserResponseDTO updateProfile(UserUpdateDTO updateDTO, String email);
+
+    AuthResponseDTO createAuthenticationToken(AuthRequestDTO authRequestDTO) throws Exception;
+
+    AuthResponseDTO createAuthRefreshToken(AuthResponseDTO responseDTO) throws Exception;
+
+    Page<UserResponseDTO> getAllUsers(Pageable pageable);
 }
+//
