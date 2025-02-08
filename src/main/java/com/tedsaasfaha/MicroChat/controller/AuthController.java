727632def06b4d8df24d80fdd38eb68d7d98ff7c
Tedros Alem <tedsaasfaha@gmail.com>
2025-02-08 13:36:42 +0300

    implement logout endpoint, logoutUser method to AuthController class for handling user logout and adds the token to the blacklist when a user logs out.

--- src/main/java/com/tedsaasfaha/MicroChat/controller/AuthController.java
+++ src/main/java/com/tedsaasfaha/MicroChat/controller/AuthController.java
@@ -2,6 +2,7 @@
 package com.tedsaasfaha.MicroChat.controller;
 
 
+import com.tedsaasfaha.MicroChat.config.TokenBlacklist;
 import com.tedsaasfaha.MicroChat.dto.AuthRequestDTO;
 import com.tedsaasfaha.MicroChat.dto.AuthResponseDTO;
 import com.tedsaasfaha.MicroChat.dto.UserRegistrationDTO;
@@ -12,10 +13,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -23,6 +21,9 @@ public class AuthController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TokenBlacklist tokenBlacklist;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signupUser(
@@ -51,6 +52,16 @@ public class AuthController {
 
         AuthResponseDTO response = userService.createAuthenticationToken(authRequestDTO);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logoutUser(
+            @RequestHeader("Authorization") String token
+    ) {
+
+        String jwt = token.substring(7);
+        tokenBlacklist.blacklistToken(jwt);
+        return ResponseEntity.ok("Logged out successfully.");
     }
 
     @PostMapping("/refresh-token")
