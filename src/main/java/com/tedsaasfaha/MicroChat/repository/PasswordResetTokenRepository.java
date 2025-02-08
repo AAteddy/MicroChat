a275fe5fd0973c7e768f8c7ba9469079905150b6
Tedros Alem <tedsaasfaha@gmail.com>
2025-02-08 19:11:26 +0300

    implement PasswordResetTokenRepository repository which handles database operations for PasswordResetToken.

--- src/main/java/com/tedsaasfaha/MicroChat/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/tedsaasfaha/MicroChat/repository/PasswordResetTokenRepository.java
@@ -1,4 +1,10 @@
 package com.tedsaasfaha.MicroChat.repository;
 
-public interface PasswordResetTokenRepository {
+import com.tedsaasfaha.MicroChat.model.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
 }
