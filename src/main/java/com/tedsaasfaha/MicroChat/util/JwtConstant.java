4e71ef549c933d0886fc935bc0ae4316941545b3
Tedros Alem <tedsaasfaha@gmail.com>
2025-02-08 23:48:27 +0300

    update JwtConstant class to add Gmail credentials securely from environment variable.

--- src/main/java/com/tedsaasfaha/MicroChat/util/JwtConstant.java
+++ src/main/java/com/tedsaasfaha/MicroChat/util/JwtConstant.java
@@ -6,8 +6,14 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class JwtConstant {
     private static final Dotenv dotenv = Dotenv.load();
 
+    // JWT Configuration
     public static final String SECRET_KEY = dotenv.get("JWT_SECRET_KEY");
     public static final long ACCESS_TOKEN_VALIDITY = 2 * 60 * 60 * 1000; // 2 hrs
     public static final long REFRESH_TOKEN_VALIDITY = 12 * 60 * 60 * 1000; // 12 hrs
+
+
+    // Gmail SMTP Configuration
+    public static final String GMAIL_USERNAME = dotenv.get("GMAIL_USERNAME", "default-email@gmail.com");
+    public static final String GMAIL_PASSWORD = dotenv.get("GMAIL_PASSWORD", "default-password");
 }
 //
