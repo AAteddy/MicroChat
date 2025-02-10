76e815a558b86556a86b89317b17a1a063fad411
Tedros Alem <tedsaasfaha@gmail.com>
2025-02-10 16:00:59 +0300

    fix depreciated warning issues for ipRateLimiter method & userRateLimiter method in RateLimitingConfig class.

--- src/main/java/com/tedsaasfaha/MicroChat/config/RateLimitingConfig.java
+++ src/main/java/com/tedsaasfaha/MicroChat/config/RateLimitingConfig.java
@@ -1,9 +1,9 @@
+
 package com.tedsaasfaha.MicroChat.config;
 
 
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,20 +15,21 @@ public class RateLimitingConfig {
     // Limit: 3 password reset requests per hour per Ip
     @Bean(name = "ipRateLimiter")
     public Bucket ipRateLimiter() {
-        Bandwidth limit = Bandwidth.classic(
-                3, // 3 requests
-                Refill.intervally(3, Duration.ofHours(1)) // Refill 3 tokens every hour
-        );
+        Bandwidth limit = Bandwidth.builder()
+                .capacity(3) // 3 requests
+                .refillGreedy(3, Duration.ofHours(1)) // Refill 3 tokens every hour
+                .build();
         return Bucket.builder().addLimit(limit).build();
     }
 
     // Limit: 5 password reset requests per hour per user
     @Bean(name = "userRateLimiter")
     public Bucket userRateLimiter() {
-        Bandwidth limit = Bandwidth.classic(
-                5, // 5 requests
-                Refill.intervally(5, Duration.ofHours(1)) // Refill 4 tokens every hour
-        );
+        Bandwidth limit = Bandwidth.builder()
+                .capacity(5)
+                .refillGreedy(5, Duration.ofHours(1))
+                .build();
         return Bucket.builder().addLimit(limit).build();
     }
 }
+//
