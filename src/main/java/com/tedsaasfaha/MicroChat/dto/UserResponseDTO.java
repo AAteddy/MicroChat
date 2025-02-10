9bc43d3bdd16f15186d8a2c2f37e5d2af0e2c09d
Tedros Alem <tedsaasfaha@gmail.com>
2025-02-10 22:10:38 +0300

    add HATEOAS links to the UserResponseDTO.

--- src/main/java/com/tedsaasfaha/MicroChat/dto/UserResponseDTO.java
+++ src/main/java/com/tedsaasfaha/MicroChat/dto/UserResponseDTO.java
@@ -1,11 +1,34 @@
 
 package com.tedsaasfaha.MicroChat.dto;
 
-public record UserResponseDTO(
-        Long id,
-        String name,
-        String email,
-        String role
-) {
+import com.tedsaasfaha.MicroChat.controller.UserController;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class UserResponseDTO extends RepresentationModel<UserResponseDTO> {
+    private Long id;
+    private String name;
+    private String email;
+    private String role;
+
+    public void addLinks() {
+        // Self link: /api/v1/users/{id}
+        this.add(WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(UserController.class)
+                        .getUserById(this.id))
+                .withSelfRel());
+
+        // Collection link: /api/v1/users
+        this.add(WebMvcLinkBuilder.linkTo(
+                WebMvcLinkBuilder.methodOn(UserController.class)
+                        .getAllActiveUsers(0, 10))
+                .withRel("users"));
+    }
 }
 //
