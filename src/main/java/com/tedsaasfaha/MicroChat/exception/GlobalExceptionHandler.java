4ea26ed80282428ad5faa9b49e82d746cb9ac7ad
Tedros Alem <tedsaasfaha@gmail.com>
2025-02-10 15:12:22 +0300

    implement GlobalExceptionHandler exception class global exception handler to return proper HTTP responses.

--- src/main/java/com/tedsaasfaha/MicroChat/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tedsaasfaha/MicroChat/exception/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.tedsaasfaha.MicroChat.exception;
 
+
+import org.apache.catalina.filters.ExpiresFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TooManyRequestsException.class)
+    public ResponseEntity<String> handleTooManyRequests(TooManyRequestsException ex) {
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                .body(ex.getMessage());
+    }
 }
